f01516523ea447035f9bb6186df68c028fa5ee7d
chhabriv <viren.chhabria@gmail.com>
2019-04-12 22:01:03 +0100

    merge

--- code/hierarchial_modelling.R
+++ code/hierarchial_modelling.R
@@ -5,8 +5,13 @@ library("readr")
 library("MCMCpack")
 
 #DATA_PATH="~/code/AppliedStatisticalModelling/Assignment3/dataset/"
+<<<<<<< HEAD
 #PATH="H:/TCD/Semester 2/AppliedStatisticalModelling/Assignments/Assignment3/Yelp-Dataset-Statistical-Modelling/"
 PATH="../dataset/"
+=======
+PATH="~/code/AppliedStatisticalModelling/Assignment3/"
+#PATH="H:/TCD/Semester 2/AppliedStatisticalModelling/Assignments/Assignment3/Yelp-Dataset-Statistical-Modelling/"
+>>>>>>> final code and changes
 DATA_FOLDER="dataset/"
 DATA_PATH=paste(PATH,DATA_FOLDER,sep="")
 PLOT_FOLDER="visuals/"
@@ -22,15 +27,15 @@ CATEGORIES="categories"
 INDIAN_TAG="Indian"
 
 save_plot=function(title){
-  ggsave(filename=paste(title,".jpeg",sep=""),plot=last_plot(),
+  ggsave(filename=paste(gsub(" ","",title),".jpeg",sep=""),plot=last_plot(),
          device="jpeg",path=VISUALS,width=12,height = 8,dpi=300)
 }
 
 INITIAL_MEAN=2.5
 INITIAL_VARIANCE=1.5625
 INITIAL_DIFFERENCE=0
-A_0=4
-B_0=1.6
+A_0=2
+B_0=2*INITIAL_VARIANCE
 ITERATIONS=5000
 KAPPA=2
 
@@ -39,7 +44,14 @@ INDIA_NEIGHBORHOOD_DISTRIBUTION="INDIAN RESTAURANT RATING DISTRIBUTION PER NEIGH
 INDIAN_NEIGHBORHOOD_RATING="INDIAN RESTAURANTS COUNT OF RATINGS PER NEIGHBORHOOD"
 HISTOGRAM_DIFF_SIM_NEIGHBORHOOD_RATINGS="INDIAN RESTAURANT SIMULATED RATING DIFFERENCE"
 MEAN_SAMPLE_RATING_VS_NUMBER_RESTAURANTS_NEIGHBORHOOD="MEAN RATING FROM SAMPLES VS NUMBER OF RESTAURANTS PER NEIGHBORHOOD"
+MEAN_SIM_RATING_NEIGHBORHOOD_SIZE="MEAN RATING FROM SAMPLES AGAINST NUMBER OF RESTAURANTS PER NEIGHBORHOOD"
 Y1SIM_VS_Y2SIM="Simulated ratings for Etobicoke(Y1) and Scarborough(Y2)"
+NEIGHBORHOOD_MEDIAN_RATING_NEIGHBORHOOD_SIZE="Median ratings of restaurants in a neighborhood by size"
+NEIGHBORHOOD_MEAN_RATING_NEIGHBORHOOD_SIZE="Mean ratings of restaurants in a neighborhood by size"
+MEAN_RATING_NEIGHBORHOOD_SIZE="Mean ratings of each neighborhood by size"
+MCMC_PLOT_2_NEIGHBORHOOD="Question1MCMC"
+HISTOGRAM_NEIGHBORHOOD_RATING_DISTRIBUTION="Histogram of Restaurant Rating Distribution"
+NEIGHBORHOOD_SIZE_RATING="Number of Restaurants per Neighborhood"
 BUSINESS_TORONTO=paste(DATA_PATH,BUSINESS_TORONTO_FILE,sep = "")
 
 business = stream_in(file(BUSINESS_TORONTO))
@@ -52,14 +64,14 @@ all_restaurant_ratings <- subset(business, neighborhood == NEIGHBORHOOD_1 | neig
 
 
 ## Make a new indicator variable for neighborhood
-restaurant_ratings$indicator <- as.numeric(factor(restaurant_ratings$neighborhood))
+all_restaurant_ratings$indicator <- as.numeric(factor(all_restaurant_ratings$neighborhood))
 
-neighborhood_vs_rating=as.data.frame(table(restaurant_ratings$neighborhood, 
-                                           restaurant_ratings$stars,dnn = c(NEIGHBORHOOD_COLUMN, STARS_COLUMN)))
+neighborhood_vs_rating=as.data.frame(table(all_restaurant_ratings$neighborhood, 
+                                           all_restaurant_ratings$stars,dnn = c(NEIGHBORHOOD_COLUMN, STARS_COLUMN)))
 ggplot(data=neighborhood_vs_rating,aes(neighborhood,stars))+
   geom_point(aes(size = Freq))+
   ggtitle(NEIGHBORHOOD_RATING_DISTRIBUTION)
-save_plot(NEIGHBORHOOD_RATING_DISTRIBUTION,last_plot())
+save_plot(NEIGHBORHOOD_RATING_DISTRIBUTION)
 
 class(business$categories)
 total_categories=unlist(business$categories)
@@ -156,7 +168,9 @@ gibbs_sampler <- function(y, ind, mu0 = INITIAL_MEAN, tau0 = 1/INITIAL_VARIANCE,
 }
 
 hierarchial_model=gibbs_sampler(indian_restaurants$stars,as.factor(indian_restaurants$indicator))
+jpeg(paste(VISUALS,MCMC_PLOT_2_NEIGHBORHOOD,".jpeg",sep=""))
 plot(as.mcmc(hierarchial_model))
+dev.off()
 raftery.diag(as.mcmc(hierarchial_model))
 apply(hierarchial_model, 2, mean)
 apply(hierarchial_model, 2, sd)
@@ -189,19 +203,32 @@ business_with_neighborhood=subset(business,business$neighborhood!="")
 business_with_neighborhood$neighborhood_indicator = as.numeric(factor(business_with_neighborhood$neighborhood))
 #Think about how to show this
 ggplot(business_with_neighborhood) + 
-  geom_boxplot(aes(x = reorder(neighborhood_indicator, stars, median), 
+  geom_boxplot(aes(x = reorder(neighborhood, stars, median), 
                                     stars, 
-                                    fill = reorder(neighborhood_indicator, stars, median)), 
-               show.legend=FALSE)
+                                    fill = reorder(neighborhood, stars, median)), 
+               show.legend=FALSE)+  
+  theme(plot.title = element_text(hjust = 0.5),
+  axis.text.x = element_text(angle = 90, hjust = 1))+
+  ggtitle(NEIGHBORHOOD_MEDIAN_RATING_NEIGHBORHOOD_SIZE)
+save_plot(NEIGHBORHOOD_MEDIAN_RATING_NEIGHBORHOOD_SIZE)
 
 ggplot(business_with_neighborhood, 
-       aes(x = reorder(neighborhood_indicator, stars, length))) + stat_count()
+       aes(x = reorder(neighborhood, stars, length))) + stat_count(fill="#619CFF",color="black")+  
+  theme(plot.title = element_text(hjust = 0.5),
+        axis.text.x = element_text(angle = 90, hjust = 1))+
+  ggtitle(NEIGHBORHOOD_SIZE_RATING)
+save_plot(NEIGHBORHOOD_SIZE_RATING)
+
 write.csv(unique(data.frame("Neighborhood"=business_with_neighborhood$neighborhood,
                        "Neighborhood Code"=business_with_neighborhood$neighborhood_indicator,
                        check.names = FALSE)),
           paste(DATA_FOLDER,NEIGHBORHOOD_MAPPING_FILENAME,sep=""))
 
-ggplot(business_with_neighborhood, aes(stars)) + stat_bin(bins = 10)
+ggplot(business_with_neighborhood, aes(stars)) + 
+  stat_bin(bins = 10,fill="#619CFF",color="black")+
+ggtitle(HISTOGRAM_NEIGHBORHOOD_RATING_DISTRIBUTION)+
+  theme(plot.title = element_text(face="bold",hjust = 0.5))
+save_plot(HISTOGRAM_NEIGHBORHOOD_RATING_DISTRIBUTION)
 
 avg_size=data.frame(size=tapply(business_with_neighborhood$stars, 
            business_with_neighborhood$neighborhood, length))
@@ -213,15 +240,24 @@ avg_mean_rating$neighborhood=row.names(avg_mean_rating)
 
 avg=merge(avg_size,avg_mean_rating,by.x=NEIGHBORHOOD_COLUMN,
           by.y=NEIGHBORHOOD_COLUMN)
+
 ggplot(avg, 
-       aes(as.numeric(factor(neighborhood)), mean_rating))+
+       aes(size, mean_rating))+
+  geom_point()+
+  geom_hline(yintercept = mean(avg$mean_rating), colour="blue", linetype="dashed")+
+  theme(plot.title = element_text(hjust = 0.5),
+        axis.text.x = element_text(angle = 90, hjust = 1))+
+  ggtitle(MEAN_RATING_NEIGHBORHOOD_SIZE)
+save_plot(MEAN_RATING_NEIGHBORHOOD_SIZE)
+
+ggplot(avg, 
+       aes(neighborhood, mean_rating))+
   geom_point(aes(size=size))+
-  scale_x_continuous(breaks=row.number(avg$size))
-
-ggplot(avg, 
-       aes(size, mean_rating,col=(factor(neighborhood))))+
-  geom_point()
-
+  geom_hline(yintercept = mean(avg$mean_rating), colour="blue", linetype="dashed")+
+  theme(plot.title = element_text(hjust = 0.5),
+        axis.text.x = element_text(angle = 90, hjust = 1))+
+  ggtitle(NEIGHBORHOOD_MEAN_RATING_NEIGHBORHOOD_SIZE)
+save_plot(NEIGHBORHOOD_MEAN_RATING_NEIGHBORHOOD_SIZE)
 
 gibbs_sampler_multiple <- function(y, ind, maxiter = ITERATIONS)
 {
@@ -297,7 +333,22 @@ apply(multi_hierarchial_model$params, 2, sd)
 mean(1/sqrt(multi_hierarchial_model$params[, 3]))
 sd(1/sqrt(multi_hierarchial_model$params[, 3]))
 theta_hat <- apply(multi_hierarchial_model$theta, 2, mean)
+
+result=data.frame(size = tapply(business_neighborhood_open$stars, business_neighborhood_open$neighborhood, length), 
+                  theta_hat = theta_hat)
+result$neighborhood=row.names(result)
+
 ggplot(data.frame(size = tapply(business_neighborhood_open$stars, business_neighborhood_open$neighborhood, length), 
                   theta_hat = theta_hat), aes(size, theta_hat)) + 
   geom_point()+
-  ggtitle(MEAN_SAMPLE_RATING_VS_NUMBER_RESTAURANTS_NEIGHBORHOOD)
+  ggtitle(MEAN_SAMPLE_RATING_VS_NUMBER_RESTAURANTS_NEIGHBORHOOD)+
+  theme(plot.title = element_text(hjust = 0.5))
+save_plot(MEAN_SAMPLE_RATING_VS_NUMBER_RESTAURANTS_NEIGHBORHOOD)
+
+ggplot(result, aes(neighborhood, theta_hat)) + 
+  geom_point(aes(size=result$size))+
+  ggtitle(MEAN_SIM_RATING_NEIGHBORHOOD_SIZE)+
+  theme(plot.title = element_text(hjust = 0.5),
+        axis.text.x = element_text(angle = 90, hjust = 1))+
+  geom_hline(yintercept = mean(result$theta_hat), colour="blue", linetype="dashed")+
+save_plot(MEAN_SIM_RATING_NEIGHBORHOOD_SIZE)
